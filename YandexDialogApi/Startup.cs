6905dc727d65e6d355558333f565d9d513728fc3
Sergey Sklyarov <sklyarov@zfort.com>
2022-01-16 09:50:25 +0200

    Disable https

--- YandexDialogApi/Startup.cs
+++ YandexDialogApi/Startup.cs
@@ -44,7 +44,7 @@ namespace YandexDialogApi
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "YandexDialogApi v1"));
             //}
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
